e9525557fbcdba191d62f44b889e7d83d72b9863
SamuelCGP <cgplays51@gmail.com>
2021-08-30 09:58:34 -0300

    primeiro upload

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD-QFgkX1GplYBpnsla3rxJihLPIc-6H6s",
+    authDomain: "cordova-app-etec.firebaseapp.com",
+    projectId: "cordova-app-etec",
+    storageBucket: "cordova-app-etec.appspot.com",
+    messagingSenderId: "384674061185",
+    appId: "1:384674061185:web:739b2e3eba97b06e6e2680",
+    measurementId: "G-YG3M0V3L3L"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
